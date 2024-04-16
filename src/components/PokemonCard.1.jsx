9f4b543421de36a043305806bd27c187c3237c20
Angéline SIMON <angelinesimon@protonmail.com>
2024-04-16 15:42:46 +0200

    react basics 04-pokedex

---
+++ src/components/PokemonCard.1.jsx
@@ -0,0 +1,13 @@
+export function PokemonCard({ pokemon }) {
+
+  return (
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,25 +1,16 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "meow",
-  },
-];
 
-function PokemonCard () { 
-const pokemon = pokemonList[0];
+function PokemonCard ({ pokemon }) { 
+
   return (
     <figure>
-      {pokemon.imgSrc ? (
+      {pokemon.imgSrc !=null ? (
       <img src={pokemon.imgSrc} alt={pokemon.name} />
       ) : (
         <p>???</p>
       )}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
-  );
+  ); 
 }
 
 export default PokemonCard;
