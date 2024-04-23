5edaca2046b74ee7c59b4a78395b054bcdabcbc0
Angéline SIMON <angelinesimon@protonmail.com>
2024-04-23 16:35:33 +0200

    react basics 08-pokedex

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,21 @@
 function Navbar({ pokemonIndex, setPokemonIndex, pokemonList }) {
+  const handlePokemonClick = (index) => {
+    setPokemonIndex(index);
+  };
+
   return (
     <>
-      <button
-        onClick={() => setPokemonIndex(pokemonIndex - 1)}
-        disabled={pokemonIndex === 0}
-      >
-        Précédent
-      </button>
-
-      <button
-        onClick={() => setPokemonIndex(pokemonIndex + 1)}
-        disabled={pokemonIndex === pokemonList.length - 1}
-      >
-        Suivant
-      </button>
+      {pokemonList.map((pokemon, index) => (
+        <button
+          key={index} 
+          onClick={() => handlePokemonClick(index)} // Appel de la fonction handlePokemonClick avec l'index
+          disabled={index === pokemonIndex} // Désactive le bouton si l'index correspond à l'index actuel
+        >
+          {pokemon.name}
+        </button>
+      ))}
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
